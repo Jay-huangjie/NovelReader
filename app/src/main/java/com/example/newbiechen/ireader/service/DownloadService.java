899c3203ce5324b7b3d7d87cf1fe7e6b5d454cc4
zohar-soul <102589617@qq.com>
2018-03-17 14:51:17 +0800

    #feat 移出/隐藏 不需要的功能

--- app/src/main/java/com/example/newbiechen/ireader/service/DownloadService.java
+++ app/src/main/java/com/example/newbiechen/ireader/service/DownloadService.java
@@ -39,8 +39,8 @@ import io.reactivex.disposables.Disposable;
 public class DownloadService extends BaseService {
     private static final String TAG = "DownloadService";
     //加载状态
-    private static final int LOAD_ERROR= -1;
-    private static final int LOAD_NORMAL= 0;
+    private static final int LOAD_ERROR = -1;
+    private static final int LOAD_NORMAL = 0;
     private static final int LOAD_PAUSE = 1;
     private static final int LOAD_DELETE = 2; //正在加载时候，用户删除收藏书籍的情况。
 
@@ -61,6 +61,7 @@ public class DownloadService extends BaseService {
     private OnDownloadListener mDownloadListener;
     private boolean isBusy = false;
     private boolean isCancel = false;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -78,18 +79,18 @@ public class DownloadService extends BaseService {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
+    public int onStartCommand(Intent intent, int flags, int startId) {
 
         //接受创建的DownloadTask
         Disposable disposable = RxBus.getInstance()
                 .toObservable(DownloadTaskBean.class)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        (event) ->  {
+                        (event) -> {
                             //判断任务是否为轮询标志
                             //判断任务是否存在，并修改任务
                             if (TextUtils.isEmpty(event.getBookId())
-                              || !checkAndAlterDownloadTask(event)){
+                                    || !checkAndAlterDownloadTask(event)) {
                                 addToExecutor(event);
                             }
                         }
@@ -104,17 +105,17 @@ public class DownloadService extends BaseService {
                         (event) -> {
                             //判断是否该数据存在加载列表中
                             boolean isDelete = true;
-                            for (DownloadTaskBean bean : mDownloadTaskQueue){
-                                if (bean.getBookId().equals(event.collBook.get_id())){
+                            for (DownloadTaskBean bean : mDownloadTaskQueue) {
+                                if (bean.getBookId().equals(event.collBook.get_id())) {
                                     isDelete = false;
                                     break;
                                 }
                             }
                             //如果不存在则删除List中的task
-                            if (isDelete){
+                            if (isDelete) {
                                 //
                                 Iterator<DownloadTaskBean> taskIt = mDownloadTaskList.iterator();
-                                while (taskIt.hasNext()){
+                                while (taskIt.hasNext()) {
                                     DownloadTaskBean task = taskIt.next();
                                     if (task.getBookId().equals(event.collBook.get_id())) {
                                         taskIt.remove();
@@ -122,7 +123,7 @@ public class DownloadService extends BaseService {
                                 }
                             }
                             //返回状态
-                            RxBus.getInstance().post(new DeleteResponseEvent(isDelete,event.collBook));
+                            RxBus.getInstance().post(new DeleteResponseEvent(isDelete, event.collBook));
                         }
                 );
         addDisposable(deleteDisp);
@@ -133,25 +134,26 @@ public class DownloadService extends BaseService {
     /**
      * 1. 查看是否任务已存在
      * 2. 修改DownloadTask的 taskName 和 list
+     *
      * @return
      */
-    private boolean checkAndAlterDownloadTask(DownloadTaskBean newTask){
+    private boolean checkAndAlterDownloadTask(DownloadTaskBean newTask) {
         boolean isExist = false;
-        for (DownloadTaskBean downloadTask : mDownloadTaskList){
+        for (DownloadTaskBean downloadTask : mDownloadTaskList) {
             //如果不相同则不往下执行，往下执行都是存在相同的情况
             if (!downloadTask.getBookId().equals(newTask.getBookId())) continue;
 
-            if (downloadTask.getStatus() == DownloadTaskBean.STATUS_FINISH){
+            if (downloadTask.getStatus() == DownloadTaskBean.STATUS_FINISH) {
                 //判断是否newTask是已完成
-                if (downloadTask.getLastChapter() == newTask.getLastChapter()){
+                if (downloadTask.getLastChapter() == newTask.getLastChapter()) {
                     isExist = true;
 
                     //发送回去已缓存
                     postMessage("当前书籍已缓存");
                 }
                 //判断，是否已完成的章节的起始点比新Task大，如果更大则表示新Task中的该章节已被加载，所以需要剪切
-                else if(downloadTask.getLastChapter() >
-                        (newTask.getLastChapter() - newTask.getBookChapterList().size())){
+                else if (downloadTask.getLastChapter() >
+                        (newTask.getLastChapter() - newTask.getBookChapterList().size())) {
                     //删除掉已经完成的章节
                     List<BookChapterBean> remainChapterBeans = newTask.getBookChapterList()
                             .subList(downloadTask.getLastChapter(),
@@ -175,7 +177,7 @@ public class DownloadService extends BaseService {
             }
         }
         //重置名字
-        if (!isExist){
+        if (!isExist) {
             String taskName = newTask.getTaskName()
                     + getString(R.string.nb_download_chapter_scope,
                     1, newTask.getLastChapter());
@@ -185,12 +187,12 @@ public class DownloadService extends BaseService {
         return isExist;
     }
 
-    private void addToExecutor(DownloadTaskBean taskEvent){
+    private void addToExecutor(DownloadTaskBean taskEvent) {
 
         //判断是否为轮询请求
         if (!TextUtils.isEmpty(taskEvent.getBookId())) {
 
-            if (!mDownloadTaskList.contains(taskEvent)){
+            if (!mDownloadTaskList.contains(taskEvent)) {
                 //加入总列表中，表示创建，修改CollBean的状态。
                 mDownloadTaskList.add(taskEvent);
             }
@@ -205,7 +207,7 @@ public class DownloadService extends BaseService {
         }
     }
 
-    private void executeTask(DownloadTaskBean taskEvent){
+    private void executeTask(DownloadTaskBean taskEvent) {
         Runnable runnable = () -> {
 
             taskEvent.setStatus(DownloadTaskBean.STATUS_LOADING);
@@ -214,12 +216,12 @@ public class DownloadService extends BaseService {
             List<BookChapterBean> bookChapterBeans = taskEvent.getBookChapters();
 
             //调用for循环，下载数据
-            for (int i=taskEvent.getCurrentChapter(); i<bookChapterBeans.size();++i) {
+            for (int i = taskEvent.getCurrentChapter(); i < bookChapterBeans.size(); ++i) {
 
                 BookChapterBean bookChapterBean = bookChapterBeans.get(i);
                 //首先判断该章节是否曾经被加载过 (从文件中判断)
                 if (BookManager
-                        .isChapterCached(taskEvent.getBookId(),bookChapterBean.getTitle())){
+                        .isChapterCached(taskEvent.getBookId(), bookChapterBean.getTitle())) {
 
                     //设置任务进度
                     taskEvent.setCurrentChapter(i);
@@ -232,22 +234,22 @@ public class DownloadService extends BaseService {
                 }
 
                 //判断网络是否出问题
-                if (!NetworkUtils.isAvailable()){
+                if (!NetworkUtils.isAvailable()) {
                     //章节加载失败
                     result = LOAD_ERROR;
                     break;
                 }
 
-                if (isCancel){
+                if (isCancel) {
                     result = LOAD_PAUSE;
                     isCancel = false;
                     break;
                 }
 
                 //加载数据
-                result = loadChapter(taskEvent.getBookId(),bookChapterBean);
+                result = loadChapter(taskEvent.getBookId(), bookChapterBean);
                 //章节加载完成
-                if (result == LOAD_NORMAL){
+                if (result == LOAD_NORMAL) {
                     taskEvent.setCurrentChapter(i);
                     postDownloadChange(taskEvent, DownloadTaskBean.STATUS_LOADING, i + "");
                 }
@@ -259,7 +261,7 @@ public class DownloadService extends BaseService {
             }
 
 
-            if (result == LOAD_NORMAL){
+            if (result == LOAD_NORMAL) {
                 //存储DownloadTask的状态
                 taskEvent.setStatus(DownloadTaskBean.STATUS_FINISH);//Task的状态
                 taskEvent.setCurrentChapter(taskEvent.getBookChapters().size());//当前下载的章节数量
@@ -267,17 +269,14 @@ public class DownloadService extends BaseService {
 
                 //发送完成状态
                 postDownloadChange(taskEvent, DownloadTaskBean.STATUS_FINISH, "下载完成");
-            }
-            else if (result == LOAD_ERROR){
+            } else if (result == LOAD_ERROR) {
                 taskEvent.setStatus(DownloadTaskBean.STATUS_ERROR);//Task的状态
                 //任务加载失败
                 postDownloadChange(taskEvent, DownloadTaskBean.STATUS_ERROR, "资源或网络错误");
-            }
-            else if (result == LOAD_PAUSE){
+            } else if (result == LOAD_PAUSE) {
                 taskEvent.setStatus(DownloadTaskBean.STATUS_PAUSE);//Task的状态
                 postDownloadChange(taskEvent, DownloadTaskBean.STATUS_PAUSE, "暂停加载");
-            }
-            else if (result == LOAD_DELETE){
+            } else if (result == LOAD_DELETE) {
                 //没想好怎么做
             }
 
@@ -296,7 +295,7 @@ public class DownloadService extends BaseService {
         mSingleExecutor.execute(runnable);
     }
 
-    private int loadChapter(String folderName,BookChapterBean bean){
+    private int loadChapter(String folderName, BookChapterBean bean) {
         //加载的结果参数
         final int[] result = {LOAD_NORMAL};
 
@@ -310,7 +309,7 @@ public class DownloadService extends BaseService {
                             //原因是Chapter的title可能重复，但是BookChapter的title不会重复
                             //BookChapter的title = 卷名 + 章节名 chapter 的 title 就是章节名。。
                             BookRepository.getInstance()
-                                    .saveChapterInfo(folderName, bean.getTitle(),chapterInfo.getBody());
+                                    .saveChapterInfo(folderName, bean.getTitle(), chapterInfo.getBody());
                         },
                         e -> {
                             //当前进度加载错误（这里需要判断是什么问题，根据相应的问题做出相应的回答）
@@ -323,8 +322,8 @@ public class DownloadService extends BaseService {
         return result[0];
     }
 
-    private void postDownloadChange(DownloadTaskBean task,int status,String msg){
-        if (mDownloadListener!= null){
+    private void postDownloadChange(DownloadTaskBean task, int status, String msg) {
+        if (mDownloadListener != null) {
             int position = mDownloadTaskList.indexOf(task);
             //通过handler,切换回主线程
             mHandler.post(
@@ -334,23 +333,23 @@ public class DownloadService extends BaseService {
         }
     }
 
-    private void postMessage(String msg){
+    private void postMessage(String msg) {
         RxBus.getInstance().post(new DownloadMessage(msg));
     }
 
-    private void post(DownloadTaskBean task){
+    private void post(DownloadTaskBean task) {
         RxBus.getInstance().post(task);
     }
 
 
     @Override
-    public boolean onUnbind(Intent intent){
+    public boolean onUnbind(Intent intent) {
 
         mDownloadListener = null;
         return super.onUnbind(intent);
     }
 
-    class TaskBuilder extends Binder implements IDownloadManager{
+    class TaskBuilder extends Binder implements IDownloadManager {
         @Override
         public List<DownloadTaskBean> getDownloadTaskList() {
             return Collections.unmodifiableList(mDownloadTaskList);
@@ -364,14 +363,14 @@ public class DownloadService extends BaseService {
         @Override
         public void setDownloadStatus(String taskName, int status) {
             //修改某个Task的状态
-            switch (status){
+            switch (status) {
                 //加入缓存队列
                 case DownloadTaskBean.STATUS_WAIT:
-                    for (int i=0; i<mDownloadTaskList.size(); ++i){
+                    for (int i = 0; i < mDownloadTaskList.size(); ++i) {
                         DownloadTaskBean bean = mDownloadTaskList.get(i);
-                        if (taskName.equals(bean.getTaskName())){
+                        if (taskName.equals(bean.getTaskName())) {
                             bean.setStatus(DownloadTaskBean.STATUS_WAIT);
-                            mDownloadListener.onDownloadResponse(i,DownloadTaskBean.STATUS_WAIT);
+                            mDownloadListener.onDownloadResponse(i, DownloadTaskBean.STATUS_WAIT);
                             addToExecutor(bean);
                             break;
                         }
@@ -380,19 +379,18 @@ public class DownloadService extends BaseService {
                 //从缓存队列中删除
                 case DownloadTaskBean.STATUS_PAUSE:
                     Iterator<DownloadTaskBean> it = mDownloadTaskQueue.iterator();
-                    while (it.hasNext()){
+                    while (it.hasNext()) {
                         DownloadTaskBean bean = it.next();
-                        if (bean.getTaskName().equals(taskName)){
+                        if (bean.getTaskName().equals(taskName)) {
                             if (bean.getStatus() == DownloadTaskBean.STATUS_LOADING
-                                    && bean.getTaskName().equals(taskName)){
+                                    && bean.getTaskName().equals(taskName)) {
                                 isCancel = true;
                                 break;
-                            }
-                            else{
+                            } else {
                                 bean.setStatus(DownloadTaskBean.STATUS_PAUSE);
                                 mDownloadTaskQueue.remove(bean);
                                 int position = mDownloadTaskList.indexOf(bean);
-                                mDownloadListener.onDownloadResponse(position,DownloadTaskBean.STATUS_PAUSE);
+                                mDownloadListener.onDownloadResponse(position, DownloadTaskBean.STATUS_PAUSE);
                                 break;
                             }
                         }
@@ -417,26 +415,27 @@ public class DownloadService extends BaseService {
     }
 
 
-
-    public interface IDownloadManager{
+    public interface IDownloadManager {
         List<DownloadTaskBean> getDownloadTaskList();
+
         void setOnDownloadListener(OnDownloadListener listener);
-        void setDownloadStatus(String taskName,int status);
+
+        void setDownloadStatus(String taskName, int status);
+
         void setAllDownloadStatus(int status);
     }
 
-    public interface OnDownloadListener{
+    public interface OnDownloadListener {
         /**
-         *
-         * @param pos : Task在item中的位置
+         * @param pos    : Task在item中的位置
          * @param status : Task的状态
-         * @param msg: 传送的Msg
+         * @param msg:   传送的Msg
          */
-        void onDownloadChange(int pos,int status,String msg);
+        void onDownloadChange(int pos, int status, String msg);
 
         /**
          * 回复
          */
-        void onDownloadResponse(int pos,int status);
+        void onDownloadResponse(int pos, int status);
     }
 }
